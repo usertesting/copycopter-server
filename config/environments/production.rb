01b239eeddc9e9b8702e4dd437b616d6ed4bd503
Paul Hepworth <paul@peppyheppy.com>
2012-07-27 23:24:16 -0700

    turned on asset compiling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Copycopter::Application.configure do
   config.serve_static_assets = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
